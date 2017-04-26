9a624c73f21d3ba098bc18915cb9f677e16f708c
zhangkaishun <576912228@qq.com>
2017-04-26 13:43:31 +0800

    webservic 客户端2

--- src/com/zks/client/WeatherClient2.java
+++ src/com/zks/client/WeatherClient2.java
@@ -1,9 +1,14 @@
 package com.zks.client;
 
+import com.zks.server.WeatherInterfaceImpl;
+import com.zks.server.WeatherInterfaceImplService;
+
 public class WeatherClient2 {
 
 	public static void main(String[] args) {
-		
+		WeatherInterfaceImplService weatherInterfaceImplService=new WeatherInterfaceImplService();
+		WeatherInterfaceImpl weatherInterfaceImplPort = weatherInterfaceImplService.getWeatherInterfaceImplPort();
+		System.out.println(weatherInterfaceImplPort.queryWeather("³É¶¼"));
 	}
 
 }
